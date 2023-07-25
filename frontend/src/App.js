ef6137c3bcf8b0212dee619b49eaf8c83c8dc180
westonludeke <westonludeke@gmail.com>
2023-07-25 14:45:06 -0500

    added basic outline to App.js of frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [url, setUrl] = useState('');
 
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
 
+  const fetchBlogs = async () => {
+    try {
+      const response = await axios.get('/api/blogs'); // Replace with your backend API URL
+      setBlogs(response.data);
+    } catch (error) {
+      console.error('Error fetching blogs:', error);
+    }
+  };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const newBlog = { title, author, url, likes: 0 };
 
+    try {
+      const response = await axios.post('/api/blogs', newBlog); // Replace with your backend API URL
+      setBlogs([...blogs, response.data]);
+      setTitle('');
+      setAuthor('');
+      setUrl('');
+    } catch (error) {
+      console.error('Error adding blog:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Blog List</h1>
+      <div>
+        <h2>Add a New Blog</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Title:</label>
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+          </div>
+          <div>
+            <label>Author:</label>
+            <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
+          </div>
+          <div>
+            <label>URL:</label>
+            <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
+          </div>
+          <button type="submit">Add Blog</button>
+        </form>
+      </div>
+      <div>
+        <h2>List of Blogs</h2>
+        <ul>
+          {blogs.map((blog) => (
+            <li key={blog._id}>
+              <h3>{blog.title}</h3>
+              <p>Author: {blog.author}</p>
+              <p>URL: {blog.url}</p>
+              <p>Likes: {blog.likes}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
