92cabb90aada3a14f1d028256690406913c8c582
westonludeke <westonludeke@gmail.com>
2023-07-25 14:48:42 -0500

    update frontend index.js and index.html

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
