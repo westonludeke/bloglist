5c962b39825bf5e1b6317170c624730fbc5a58a4
westonludeke <westonludeke@gmail.com>
2023-07-26 09:01:04 -0500

    fix typos and add semicolons mongo.js

--- backend/mongo.js
+++ backend/mongo.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+  console.log('give password as argument');
+  process.exit(1);
 }
 
 const password = process.argv[2];
@@ -11,7 +11,7 @@ const password = process.argv[2];
 const url = process.env.MONGO_DB_URL;
 
 mongoose.set('strictQuery',false);
-mongoose.connect(url)''
+mongoose.connect(url);
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -20,16 +20,16 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 });
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model('Blog', blogSchema);
 
 const blog = new Blog({
   title: "PG's Essays",
   author: "Paul Graham",
   url: "http://www.paulgraham.com/articles.html",
   likes: 500
-})
+});
 
 blog.save().then(result => {
-  console.log('blog saved!')
-  mongoose.connection.close()
-})
+  console.log('blog saved!');
+  mongoose.connection.close();
+});
