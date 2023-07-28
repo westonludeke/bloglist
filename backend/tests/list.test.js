2144165ccc77376e5325f6e3ec6c5b5a814c3ba5
westonludeke <westonludeke@gmail.com>
2023-07-28 11:45:19 -0500

    add favorite blog test

--- backend/tests/list.test.js
+++ backend/tests/list.test.js
@@ -49,3 +49,67 @@ describe('total likes', () => {
     expect(result).toBe(18);
   });
 });
+
+describe('favorite blog', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithThreeBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f9',
+      title: 'React is Awesome',
+      author: 'John Smith',
+      url: 'http://www.example.com/react-awesome',
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f7',
+      title: 'JavaScript Basics',
+      author: 'Jane Doe',
+      url: 'http://www.example.com/js-basics',
+      likes: 3,
+      __v: 0,
+    },
+  ];
+
+  test('when list has only one blog, it is the favorite', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    expect(result).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5,
+    });
+  });
+
+  test('when list has three blogs, it finds the favorite', () => {
+    const result = listHelper.favoriteBlog(listWithThreeBlogs);
+    expect(result).toEqual({
+      title: 'React is Awesome',
+      author: 'John Smith',
+      likes: 10,
+    });
+  });
+
+  test('when list is empty, returns null', () => {
+    const result = listHelper.favoriteBlog([]);
+    expect(result).toBeNull();
+  });
+});
+
--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -2,6 +2,26 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  let favorite = blogs[0];
+
+  blogs.forEach((blog) => {
+    if (blog.likes > favorite.likes) {
+      favorite = blog;
+    }
+  });
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
 module.exports = {
-  totalLikes,
+  favoriteBlog, totalLikes,
 };
