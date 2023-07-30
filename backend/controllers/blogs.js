65a4729ef5c9cda57258dfcec5d826d614663eb4
westonludeke <westonludeke@gmail.com>
2023-07-30 16:50:49 -0500

    Put get/ route back in controllers/blogs.js. Update frontend's src/services/blogService baseUrl to attempt fix on Render

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,7 +1,7 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/api/blogs', async (request, response) => {
+blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
 });
--- frontend/src/services/blogService.js
+++ frontend/src/services/blogService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3003/api/blogs';
+const baseUrl = '/api/blogs';
 
 const getAll = () => {
   const request = axios.get(baseUrl);
