7c575b17c3dc4e8b3f7a138f4cc84935ed6e3387
westonludeke <westonludeke@gmail.com>
2023-07-31 11:58:18 -0500

    refactor tests to use describe blocks to better organize test groups

--- backend/tests/list_api.test.js
+++ backend/tests/list_api.test.js
@@ -7,137 +7,142 @@ const Blog = require('../models/blog');
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-
-  for (let blog of helper.initialBlogs){
-    let blogObject = new Blog(blog);
-    await blogObject.save();    
-  }
+  await Blog.insertMany(helper.initialBlogs);
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs');
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  })
+
+  test("a specific blog is within the returned blogs", async () => {
+    const response = await api.get('/api/blogs');
+
+    const titles = response.body.map(r => r.title);
+    expect(titles).toContain("PG's Essays");
+  })
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs');
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+describe('viewing a specific blog', () => {
+  test('a specific blog can be viewed', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToView = blogsAtStart[0];
+
+    const resultBlog = await api
+      .get(`/api/blogs/${blogToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+    expect(resultBlog.body).toEqual(blogToView);
+  })
 })
 
-test("a specific blog is within the returned blogs", async () => {
-  const response = await api.get('/api/blogs');
+describe('addition of a new blog', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      "title": "Borderland Beat",
+      "author": "Sol Prendido",
+      "url": "borderlandbeat.com",
+      "likes": "226"
+    }
 
-  const titles = response.body.map(r => r.title);
-  expect(titles).toContain("PG's Essays");
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+    const titles = blogsAtEnd.map(n => n.title);
+    expect(titles).toContain(
+      'Borderland Beat'
+    );
+  })
+
+  test('a blog without likes can be added', async () => {
+    const newBlog = {
+      "title": "Passion of the Weiss",
+      "author": "Jeff Weiss",
+      "url": "passionweiss.com"
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+    const titles = blogsAtEnd.map(n => n.title);
+    expect(titles).toContain(
+      'Passion of the Weiss'
+    );
+  })
+
+  test('blog without title is not added', async () => {
+    const newBlog = {
+      "author": "Big Ghost LTD.",
+      "url": "http://bigghostnahmean.blogspot.com/",
+      "likes": "1000"
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  })
+
+  test('blog without URL is not added', async () => {
+    const newBlog = {
+      "title": "Daring Fireball",
+      "author": "John Gruber",
+      "likes": "1000"
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  })
+
 })
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    "title": "Borderland Beat",
-    "author": "Sol Prendido",
-    "url": "borderlandbeat.com",
-    "likes": "226"
-  }
+describe('deletion of a blog', () => {
+  test('a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    const blogsAtEnd = await helper.blogsInDb();
 
-  const titles = blogsAtEnd.map(n => n.title);
-  expect(titles).toContain(
-    'Borderland Beat'
-  );
-})
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
 
-test('blog without title is not added', async () => {
-  const newBlog = {
-    "author": "Big Ghost LTD.",
-    "url": "http://bigghostnahmean.blogspot.com/",
-    "likes": "1000"
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
-})
-
-test('a blog without likes can be added', async () => {
-  const newBlog = {
-    "title": "Passion of the Weiss",
-    "author": "Jeff Weiss",
-    "url": "passionweiss.com"
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-
-  const titles = blogsAtEnd.map(n => n.title);
-  expect(titles).toContain(
-    'Passion of the Weiss'
-  );
-})
-
-test('blog without URL is not added', async () => {
-  const newBlog = {
-    "title": "Daring Fireball",
-    "author": "John Gruber",
-    "likes": "1000"
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
-})
-
-test('a specific blog can be viewed', async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToView = blogsAtStart[0];
-
-  const resultBlog = await api
-    .get(`/api/blogs/${blogToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-
-  expect(resultBlog.body).toEqual(blogToView);
-})
-
-test('a blog can be deleted', async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToDelete = blogsAtStart[0];
-
-  await api
-    .delete(`/api/blogs/${blogToDelete.id}`)
-    .expect(204)
-
-  const blogsAtEnd = await helper.blogsInDb();
-
-  expect(blogsAtEnd).toHaveLength(
-    helper.initialBlogs.length - 1
-  )
-
-  const titles = blogsAtEnd.map(r => r.title);
-  expect(titles).not.toContain(blogToDelete.title);
+    const titles = blogsAtEnd.map(r => r.title);
+    expect(titles).not.toContain(blogToDelete.title);
+  })
 })
 
 // -----
