0de289895670bfe81c8924fd41affcedb941a53e
westonludeke <westonludeke@gmail.com>
2023-07-31 10:12:12 -0500

    fix list_api.test.js punctuation

--- backend/tests/list_api.test.js
+++ backend/tests/list_api.test.js
@@ -6,31 +6,30 @@ const api = supertest(app);
 const Blog = require('../models/blog');
 
 beforeEach(async () => {
-  await Blog.deleteMany({})
+  await Blog.deleteMany({});
   let blogObject = new Blog(helper.initialBlogs[0]);
-  await blogObject.save()
+  await blogObject.save();
   blogObject = new Blog(helper.initialBlogs[1]);
-  await blogObject.save()
+  await blogObject.save();
 })
 
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
-    .expect('Content-Type', /application\/json/)
+    .expect('Content-Type', /application\/json/);
 })
 
 test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-
+  const response = await api.get('/api/blogs');
   expect(response.body).toHaveLength(helper.initialBlogs.length);
 })
 
 test("a specific blog is within the returned blogs", async () => {
-  const response = await api.get('/api/blogs')
+  const response = await api.get('/api/blogs');
 
-  const titles = response.body.map(r => r.title)
-  expect(titles).toContain("PG's Essays")
+  const titles = response.body.map(r => r.title);
+  expect(titles).toContain("PG's Essays");
 })
 
 test('a valid blog can be added', async () => {
@@ -45,15 +44,15 @@ test('a valid blog can be added', async () => {
     .post('/api/blogs')
     .send(newBlog)
     .expect(201)
-    .expect('Content-Type', /application\/json/)
+    .expect('Content-Type', /application\/json/);
 
   const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
 
   const titles = blogsAtEnd.map(n => n.title);
   expect(titles).toContain(
     'Borderland Beat'
-  )
+  );
 })
 
 test('blog without title is not added', async () => {
@@ -66,7 +65,7 @@ test('blog without title is not added', async () => {
   await api
     .post('/api/blogs')
     .send(newBlog)
-    .expect(400)
+    .expect(400);
 
   const blogsAtEnd = await helper.blogsInDb();
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
@@ -75,5 +74,5 @@ test('blog without title is not added', async () => {
 // -----
 
 afterAll(async () => {
-  await mongoose.connection.close()
+  await mongoose.connection.close();
 })
