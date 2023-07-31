69e326d97ebe9df2a41a58ba2e42bdc82651b2f5
westonludeke <westonludeke@gmail.com>
2023-07-31 14:36:33 -0500

    Add new controllers/users.js file and update app.js file to use usersRouter

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 require('express-async-errors');
 const app = express();
 const cors = require('cors');
-const blogsRouter = require('./controllers/blogs')
+const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -14,20 +15,21 @@ logger.info('connecting to', config.MONGO_DB_URL);
 
 mongoose.connect(config.MONGO_DB_URL)
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info('connected to MongoDB');
   })
   .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
+    logger.error('error connecting to MongoDB:', error.message);
   })
 
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
-app.use(middleware.requestLogger)
+app.use(middleware.requestLogger);
 
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
