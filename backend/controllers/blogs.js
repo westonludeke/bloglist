3789ccb0a0273be8be49265e8698b71f4e75938c
westonludeke <westonludeke@gmail.com>
2023-07-31 16:13:08 -0500

    update controllers/users.js for creating a new note so the note is assigned to the user who created it

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({});
@@ -17,15 +18,20 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
+  const user = await User.findById(body.userId);
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: userId
   });
 
  const savedBlog = await blog.save();
+ user.blogs = user.blogs.concat(savedBlog._id);
+ await user.save();
+ 
  response.status(201).json(savedBlog);
 });
 
