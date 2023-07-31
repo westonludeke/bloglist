be191babc9fdafc8a59e1b953ba36e679c905097
westonludeke <westonludeke@gmail.com>
2023-07-31 16:19:55 -0500

    fix typo in blogSchema of controllers/users.js

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -25,7 +25,7 @@ blogsRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: userId
+    user: user.id
   });
 
  const savedBlog = await blog.save();
