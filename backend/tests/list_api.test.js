f42a05a9a39d17f1de60a3381bf74e1368465742
westonludeke <westonludeke@gmail.com>
2023-07-31 10:35:40 -0500

    Add tests for a 'specific blog can be viewed' and

--- backend/tests/list_api.test.js
+++ backend/tests/list_api.test.js
@@ -71,6 +71,36 @@ test('blog without title is not added', async () => {
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
 })
 
+test('a specific blog can be viewed', async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToView = blogsAtStart[0];
+
+  const resultBlog = await api
+    .get(`/api/blogs/${blogToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+
+  expect(resultBlog.body).toEqual(blogToView);
+})
+
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  expect(blogsAtEnd).toHaveLength(
+    helper.initialBlogs.length - 1
+  )
+
+  const titles = blogsAtEnd.map(r => r.title);
+  expect(titles).not.toContain(blogToDelete.title);
+})
+
 // -----
 
 afterAll(async () => {
