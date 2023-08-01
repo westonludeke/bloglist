0205cf8e528371cb220dac2585861c6ece5cfeff
westonludeke <westonludeke@gmail.com>
2023-08-01 11:11:13 -0500

    Update controllers/blogs.js to limit creating of new notes if request has a valid token attached. Use Bearer scheme.

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -31,14 +31,14 @@ blogsRouter.post('/', async (request, response) => {
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   } 
-  const user = await User.findById(body.userId);
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: user.id
+    user: user._id
   });
 
  const savedBlog = await blog.save();
