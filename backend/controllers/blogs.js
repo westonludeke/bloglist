9ac017f4d3a27e2768d4bc36008c569c89f9599e
westonludeke <westonludeke@gmail.com>
2023-08-01 10:47:44 -0500

    Update controllers/blog/js and middleware.js to limit creating of new blogs to logged-in users

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
@@ -18,6 +27,10 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  } 
   const user = await User.findById(body.userId);
 
   const blog = new Blog({
--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,12 +1,13 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const loginRouter = require('express').Router()
-const User = require('../models/user')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const loginRouter = require('express').Router();
+const User = require('../models/user');
 
 loginRouter.post('/', async (request, response) => {
-  const { username, password } = request.body
+  const { username, password } = request.body;
 
-  const user = await User.findOne({ username })
+  const user = await User.findOne({ username });
+  console.log('user: ', user);
   const passwordCorrect = user === null
     ? false
     : await bcrypt.compare(password, user.passwordHash)
@@ -22,11 +23,11 @@ loginRouter.post('/', async (request, response) => {
     id: user._id,
   }
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  const token = jwt.sign(userForToken, process.env.SECRET);
 
   response
     .status(200)
-    .send({ token, username: user.username, name: user.name })
+    .send({ token, username: user.username, name: user.name });
 })
 
-module.exports = loginRouter
+module.exports = loginRouter;
