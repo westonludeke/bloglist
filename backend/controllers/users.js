47037669f9b9e4d7b25abc7f32cb874d79e1bfa1
westonludeke <westonludeke@gmail.com>
2023-08-02 11:32:39 -0500

    Add GET request to display individual user's info

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,7 +5,16 @@ const User = require('../models/user');
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({}).populate('blogs', { title: 1, author: 1 });
   response.json(users);
-})
+});
+
+usersRouter.get('/:id', async (request, response) => {
+  const user = await User.findById(request.params.id);
+  if (user){
+    response.json(user);
+  } else {
+    response.status(404).end();
+  }
+});
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body;
