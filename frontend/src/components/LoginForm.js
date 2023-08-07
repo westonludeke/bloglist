f81b0b06c203be5c99a61e55645eeccbcfea171e
westonludeke <westonludeke@gmail.com>
2023-08-07 09:07:48 -0500

    Small ESLint fixes

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -4,31 +4,31 @@ const LoginForm = ({
   handlePasswordChange,
   username,
   password
- }) => {
- return (
-   <div>
-     <h2>Login</h2>
+}) => {
+  return (
+    <div>
+      <h2>Login</h2>
 
-     <form onSubmit={handleSubmit}>
-       <div>
-         username
-         <input
-           value={username}
-           onChange={handleUsernameChange}
-         />
-       </div>
-       <div>
-         password
-         <input
-           type="password"
-           value={password}
-           onChange={handlePasswordChange}
-         />
-     </div>
-       <button type="submit">login</button>
-     </form>
-   </div>
- )
-}
+      <form onSubmit={handleSubmit}>
+        <div>
+          username
+          <input
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+};
 
-export default LoginForm
+export default LoginForm;
--- frontend/src/services/blogService.js
+++ frontend/src/services/blogService.js
@@ -6,38 +6,38 @@ let token = null;
 
 const setToken = newToken => {
   token = `Bearer ${newToken}`;
-}
+};
 
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then(response => response.data);
-}
+};
 
 const create = async newObject => {
   const config = {
     headers: { Authorization: token },
-  }
-  const response = await axios.post(baseUrl, newObject, config)
+  };
+  const response = await axios.post(baseUrl, newObject, config);
   return response.data;
-}
+};
 
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject);
   return request.then(response => response.data);
-}
+};
 
 const remove = async (id) => {
   try {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`, // Include the Bearer token in the Authorization header
-    },
-  };
-  await axios.delete(`${baseUrl}/${id}`, config);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`, // Include the Bearer token in the Authorization header
+      },
+    };
+    await axios.delete(`${baseUrl}/${id}`, config);
   } catch (error){
-    console.log('blogService Error deleting blog: ', error)
-  } 
+    console.log('blogService Error deleting blog: ', error);
+  }
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { getAll, create, update, remove, setToken }
+export default { getAll, create, update, remove, setToken };
