1fd1e2e67bf5b042ed77f50d9d9894bd7d89158d
westonludeke <westonludeke@gmail.com>
2023-08-07 10:31:14 -0500

    Center login header above login form on LoginForm.js

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -6,26 +6,28 @@ const LoginForm = ({
   password
 }) => {
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h2>Login</h2>
 
       <form onSubmit={handleSubmit}>
         <div>
-          username
+          username:
           <input
             value={username}
             onChange={handleUsernameChange}
           />
         </div>
         <div>
-          password
+          password:
           <input
             type="password"
             value={password}
             onChange={handlePasswordChange}
           />
         </div>
-        <button type="submit">login</button>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+          <button type="submit">login</button>
+        </div>
       </form>
     </div>
   );
