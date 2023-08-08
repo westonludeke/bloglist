60775ee26097ff012488692f6a595e58c69e4f6b
westonludeke <westonludeke@gmail.com>
2023-08-08 15:03:22 -0500

    Install eslint-plugin-cypress development dependency

---
+++ frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,7 @@
+describe('Blog app', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:3000')
+    cy.contains('Blog')
+    cy.contains('Cool Tech Blogs!')
+  })
+})
