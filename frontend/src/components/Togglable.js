9a83eadce0a7b51c5f6b107255b74448868a36da
westonludeke <westonludeke@gmail.com>
2023-08-08 09:46:18 -0500

    Attempt to fix hiding of add new blog form after new blog is successfully added. Changes to Togglable.js component

--- frontend/src/components/Togglable.js
+++ frontend/src/components/Togglable.js
@@ -3,24 +3,23 @@ import { useState } from 'react';
 const Togglable = (props) => {
   const [visible, setVisible] = useState(false);
 
-  const hideWhenVisible = { display: visible ? 'none' : '' };
-  const showWhenVisible = { display: visible ? '' : 'none' };
-
   const toggleVisibility = () => {
     setVisible(!visible);
   };
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <div style={hideWhenVisible}>
+      <div style={{ display: visible ? 'none' : '' }}>
         <button onClick={toggleVisibility}>{props.buttonLabel}</button>
       </div>
-      <div style={{ ...showWhenVisible, marginTop: '10px' }} className="togglableContent">
-        {props.children}
-        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-          <button onClick={toggleVisibility}>cancel</button>
+      {visible && (
+        <div style={{ marginTop: '10px' }} className="togglableContent">
+          {props.children}
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+            <button onClick={toggleVisibility}>cancel</button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
