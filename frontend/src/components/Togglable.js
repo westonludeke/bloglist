9a5e63d8aad317beb6aa99ba0682d4d402e8a661
westonludeke <westonludeke@gmail.com>
2023-08-08 11:30:03 -0500

    update Togglable.js

--- frontend/src/components/Togglable.js
+++ frontend/src/components/Togglable.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useImperativeHandle, forwardRef } from 'react';
+import PropTypes from 'prop-types';
 
-const Togglable = (props) => {
+const Togglable = forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false);
 
   const hideWhenVisible = { display: visible ? 'none' : '' };
@@ -9,6 +10,12 @@ const Togglable = (props) => {
   const toggleVisibility = () => {
     setVisible(!visible);
   };
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleVisibility
+    };
+  });
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
@@ -23,6 +30,12 @@ const Togglable = (props) => {
       </div>
     </div>
   );
+});
+
+Togglable.displayName = 'Togglable';
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
 };
 
 export default Togglable;
